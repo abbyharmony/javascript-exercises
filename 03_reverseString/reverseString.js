ec66012e5463680e8bcb70441399254f7f22f2a9
abbyharmony <abbyharmony@gmail.com>
2024-03-06 19:23:55 -0500

    Completed 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+   let splitStr = str.split("");
+   let reversedStr = splitStr.reverse();
+   let joinedStr = reversedStr.join("");
+   return joinedStr;
 };
 
 // Do not edit below this line
