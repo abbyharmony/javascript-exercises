a668a1227363e54ef08f3896015b46706daef030
abbyharmony <abbyharmony@gmail.com>
2024-03-07 21:26:53 -0500

    Complete 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (typeof min !== "number" || typeof max !== "number") {
+        return "ERROR";
+    } else if (min > max) {
+        [min, max] = [max, min];
+    } else if (min < 0 || max < 0) {
+        return "ERROR";
+    }
+    
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
