c6d425f0d8c9f089a5d05e8b5734c8b57d831730
abbyharmony <abbyharmony@gmail.com>
2024-03-07 22:11:09 -0500

    Complete 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let convertedTemp = (temp - 32) * 5/9;
+  let rounded = Math.round(convertedTemp * 10) / 10;
+  let fixed = rounded.toFixed(1);
+  return parseFloat(convertedTemp.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temps) {
+  let convertedTemps = (temps * 9/5 + 32);
+  let rounded = Math.round(convertedTemps * 10) / 10;
+  let fixed = rounded.toFixed(1);
+  return parseFloat(convertedTemps.toFixed(1));
 };
 
 // Do not edit below this line
